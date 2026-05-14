3e1fd15854f3afef6494444a4720596414351b75
User <user@Users-MacBook-Pro.local>
2026-05-14 21:46:04 +0100

    fresh backend

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend online');
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello from Render backend!'
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server running');
+});
