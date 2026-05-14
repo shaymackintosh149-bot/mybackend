4d97baa68172ee0e70c60b82c76effbcbec713da
User <user@Users-MacBook-Pro.local>
2026-05-14 21:50:24 +0100

    update backend

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.get('/api', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Server running');
+  console.log('Server running on port ' + PORT);
 });
